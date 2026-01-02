3754ce857c5f84c0069c9be6149f725ddad2b084
subhasmitanayak18 <subhasmitanayak2005@gmail.com>
2026-01-02 22:42:45 +0530

    Add ML-based AQI prediction model

--- src/aqi_ml.py
+++ src/aqi_ml.py
@@ -1,5 +1,26 @@
 import pandas as pd
-data = pd.read_csv(r"C:\OLIVIA\PYTHONFOLDER\project\data\air_quality_data.csv")
-print(data.head())
-print("\nDataset info:",data.info())
-print("\nStatistical Summary:",data.describe())
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+
+df = pd.read_csv(r"C:\OLIVIA\PYTHONFOLDER\project\data\air_quality_data.csv")
+df = df.dropna()
+print(df.head())
+print(df.info())
+X = df[['PM2.5', 'PM10', 'Temperature', 'Humidity']]
+y = df['AQI']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Absolute Error:", mae)
+print("R2 Score:", r2)
